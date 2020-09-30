4d98893d5a99a374adb1ac2fd7ff06a74b071081
nballance <59257894+nballance@users.noreply.github.com>
2020-09-30 17:59:25 -0400

    Update PartialHTTP1Server.java

--- PartialHTTP1Server.java
+++ PartialHTTP1Server.java
@@ -1,7 +1,59 @@
+import java.net.*;
+import java.io.*;
 
 public class PartialHTTP1Server {
-	
-	public static void main (String[]args) {
-		System.out.println("Hello World");
-	}
+
+    //public static void main (String[]args) {}
+
+    //initialize socket and input stream
+    private Socket          socket   = null;
+    private ServerSocket    server   = null;
+    private DataInputStream in       =  null;
+
+    // constructor with port
+    public PartialHTTP1Server(int port){
+        // starts server and waits for a connection
+    try{
+        server = new ServerSocket(port);
+        System.out.println("Server started");
+
+        System.out.println("Waiting for a client ...");
+
+        //Accept blocking call
+        socket = server.accept();
+
+        System.out.println("Client accepted");
+
+        // takes input from the client socket
+        in = new DataInputStream(
+        new BufferedInputStream(socket.getInputStream()));
+
+        String line = "";
+
+        // reads message from client until "Over" is sent
+        while (!line.equals("\n\n")){
+            try{
+                line = in.readUTF();
+                System.out.println(line);
+
+            }catch(IOException i){
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Closing connection");
+
+        // close connection
+        socket.close();
+        in.close();
+    }catch(IOException i){
+        System.out.println(i);
+    }
+    }
+
+
+    public static void main(String args[]){
+        PartialHTTP1Server server = new PartialHTTP1Server(Integer.parseInt(args[0]));
+    }
+
 }
