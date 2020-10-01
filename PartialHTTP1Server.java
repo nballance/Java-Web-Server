672abe380ccc16370e9efe1baca4bf9da6c5f383
Spaghetti-Man-Code <62676554+Spaghetti-Man-Code@users.noreply.github.com>
2020-09-30 21:11:19 -0400

    Update PartialHTTP1Server.java

--- PartialHTTP1Server.java
+++ PartialHTTP1Server.java
@@ -6,119 +6,76 @@ import java.io.*;
 
 public class PartialHTTP1Server {
 
-	//public static void main (String[]args) {}
+        public static void main(String args[]){
 
-	//initialize socket and input stream
-	private Socket          socket   = null;
-	private ServerSocket    server   = null;
-	private DataInputStream in       =  null;
+                //initialize socket and input stream
+                private Socket socket = null;
+                private ServerSocket server = null;
+                private DataInputStream din = null;
+                private DataOutputStream dos = null;
 
-	// constructor with port
-	public PartialHTTP1Server(int port){
-		// starts server and waits for a connection
-		try{
-			server = new ServerSocket(port);
-			System.out.println("Server started");
+                private ArrayList<Thread> thread_ids = new ArrayList<Thread>(50);
 
-			System.out.println("Waiting for a client ...");
+                try{
+                        server = new ServerSocket(Integer.parseInt(args[0]));
+                        System.out.println("Server started");
 
-			//Accept blocking call
-			socket = server.accept();
+                        System.out.println("Waiting for a client ...");
 
-			System.out.println("Client accepted");
+                        while(true){
+                                //Accept blocking call
+                                socket = server.accept();
 
-			// takes input from the client socket
-			in = new DataInputStream(
-					new BufferedInputStream(socket.getInputStream()));
+                                System.out.println("Client accepted");
 
-			String line = "";
+                                // setting input and output streams
+                                din = new DataInputStream(socket.getInputStream());
+                                dos = new DataOutputStream(socket.getOutputStream());
 
-			// reads message from client until "Over" is sent
-			while (!line.equals("\n\n")){
-				try{
-					line = in.readUTF();
-					System.out.println(line);
+                                System.out.println("Assigning new thread for this client");
 
-				}catch(IOException i){
-					System.out.println(i);
-				}
-			}
+                                thread_ids.add(new ClientHandler(socket, dis, dos));
 
-			System.out.println("Closing connection");
+                        }
 
-			// close connection
-			socket.close();
-			in.close();
-		}catch(IOException i){
-			System.out.println(i);
-		}
-	}
-	
-	public static void myGETRequest(String resource) throws IllegalArgumentException{
-		//Invalid URL
-		
-		//Invalid file path
-		
-		//Invalid format
-		
-		//Check Date?
-		throw new IllegalArgumentException();
-	}
-	
-	public static void myPOSTRequest(String resource) throws IllegalArgumentException{
-		throw new IllegalArgumentException();
-	}
-	
-	public static void myHEADRequest(String resource) throws IllegalArgumentException{
-		throw new IllegalArgumentException();
-	}
-	
+                System.out.println("Stopping Accept...");
 
-	public static void main(String args[]) {
-		PartialHTTP1Server server = new PartialHTTP1Server(Integer.parseInt(args[0]));
-		
-		String threadStr = "";
-		
-		boolean isCommand=false, isResource=false, isHTTPVersion=false;
-		
-		List<String> tokens = new ArrayList<>();
-	    StringTokenizer tokenizer = new StringTokenizer(threadStr, " ");
-	    while (tokenizer.hasMoreElements()) {
-	        tokens.add(tokenizer.nextToken());
-	    }
-	    
-	    String command = "", resource = "", httpVersion ="";
-	   
-	    if(tokenizer.hasMoreElements()) {
-	    	command = (String)tokenizer.nextElement().toString();
-	    	isCommand = true;
-	    }
-	    if(tokenizer.hasMoreElements()) {
-	    	resource = (String)tokenizer.nextElement().toString();
-	    	isResource = true; 
-	    }
-	    if(tokenizer.hasMoreElements()) {
-	    	httpVersion = (String)tokenizer.nextElement().toString();
-	    	isHTTPVersion = true;
-	    }
-		
-			
-		switch(command) {
-			case "GET":
-				System.out.println("GET command");
-				myGETRequest(resource);
-				break;
-			case "POST":
-				System.out.println("POST command");
-				myPOSTRequest(resource);
-				break;
-			case "HEAD":
-				System.out.println("HEAD command");
-				myHEADRequest(resource);
-				break;
-			default:
-				System.out.println("Unsupported command");
-		}
-	}
+                // close connection
+                socket.close();
+                din.close();
+                dos.close();
+
+                }catch(IOException i){
+                        System.out.println(i);
+                }
+        }
+
+        class ClientHandler extends Thread{
+                final DataInputStream dis;
+                final DataOutputStream dos;
+                final Socket socket;
+
+                public ClientHandler(Socket socket, DataInputStream dis, DataOutputStream dos){
+                        this.socket = socket;
+                        this.dis = dis;
+                        this.dos = dos;
+                }
+
+                @Override
+                public void run(){
+                //Does something
+                }
+
+                //terminate the thread:
+
+                try{
+                        this.dis.close();
+                        this.dos.close();
+                        this.socket.close();
+
+                }catch(IOException e){
+                        e.printStackTrace();
+                }
+        }
 
 }
